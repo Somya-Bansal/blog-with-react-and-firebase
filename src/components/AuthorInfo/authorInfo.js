fb8f23b7f8756bfc1c8701324b966c38d9386c82
Somya Bansal <somyabansal1996@gmail.com>
2019-02-26 20:53:14 +0530

    styling fixes

--- src/components/AuthorInfo/authorInfo.js
+++ src/components/AuthorInfo/authorInfo.js
@@ -9,13 +9,15 @@ class AuthorInfo extends React.Component {
     render () {
         return (
             <div className={styles.AuthorInfoContainer}>
-                <div className={styles.authorInfo}>
-                    <div className={styles.authorImage}>
-                        <img src={myPic} alt=""></img>
+                <div className={styles.author}>
+                    <div className={styles.authorInfo}>
+                        <div className={styles.authorImage}>
+                            <img src={myPic} alt=""></img>
+                        </div>
+                        <h2 className={styles.authorTitle}>
+                            Somya Bansal
+                        </h2>
                     </div>
-                    <h2 className={styles.authorTitle}>
-                        Somya Bansal
-                    </h2>
                     <Button onClickHandle={this.props.logoutHandle}>Logout</Button>
                 </div>
                 <div>
