6ca8d0450d9145b738cc5405063aa10480bb3181
Somya Bansal <somyabansal1996@gmail.com>
2019-03-08 12:53:05 +0530

    config changes at deployment

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,8 +1,7 @@
 import * as firebase from "firebase";
 
 const config = {
-    // apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    apiKey: "AIzaSyDHOUJgh3cxK3ial6yzo78dAIKcMBZj_54",
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
     databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
