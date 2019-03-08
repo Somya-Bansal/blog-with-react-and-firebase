b0982670608563da4a1cc1f857bddd075c89323b
Somya Bansal <somyabansal1996@gmail.com>
2019-03-08 12:32:42 +0530

    key check

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,7 +1,8 @@
 import * as firebase from "firebase";
 
 const config = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    // apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    apiKey: "AIzaSyDHOUJgh3cxK3ial6yzo78dAIKcMBZj_54",
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
     databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
