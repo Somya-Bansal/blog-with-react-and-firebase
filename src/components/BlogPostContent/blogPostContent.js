18a7f35956c1127ed935bd267343d1eba24a0955
Somya Bansal <somyabansal1996@gmail.com>
2019-04-02 12:56:08 +0530

    avoid db call where possible

--- src/components/BlogPostContent/blogPostContent.js
+++ src/components/BlogPostContent/blogPostContent.js
@@ -1,70 +1,51 @@
 import React from "react"
-import firebase from "../../config/firebase"
-
 import styles from "./blogPostContent.module.scss"
 import blogPic from "../../images/blog1.jpg"
+import { connect } from 'react-redux';
 
 class BlogPostContent extends React.Component {
-    _isMounted = false;
-    constructor(props) {
-        super(props);
-        this.state = {
-            post: {}
-        }
-    }
-    componentDidMount() {
-        this._isMounted = true;
-        const postId = this.props.postState.id;
-        const postRef = firebase.database().ref('posts/' + postId);
-        postRef.on("value", (snapshot) => {
-            let post = snapshot.val();
-            if(this._isMounted) {
-                this.setState({
-                    post: post
-                });
-            }
-        }, function (errorObject) {
-            console.log("The read failed: " + errorObject.code);
-          }
-        );
-    }
-    componentWillUnmount() {
-        this._isMounted = false;
-    }
     render() {
+        const postId = this.props.postState.id;
+        let currentPost = this.props.blogPosts.find(blogPost => blogPost.id === postId);
+
         return (
             <div className={styles.postContainer}>
                 <h1 className={styles.postHeading}>
-                    {this.state.post.title}
-                    </h1>
+                    {currentPost.title}
+                </h1>
                 <div className={styles.postMeta}>
                     <div className={styles.author}>
                         <span>by </span>
                         <span className={styles.authorName}>
-                            {this.state.post.authorName}
+                            {currentPost.authorName}
                         </span>
                     </div>
                     <div className={styles.date}>
-                        {this.state.post.publishDate}
+                        {currentPost.publishDate}
                     </div>
                     <div className={styles.comments}>
-                        {this.state.post.commentCount} comments
+                        {currentPost.commentCount} comments
                     </div>
                     <div className={styles.upvotes}>
-                        {this.state.post.upvotes} upvotes
+                        {currentPost.upvotes} upvotes
                     </div>
                     <div className={styles.downvotes}>
-                        {this.state.post.downvotes} downvotes
+                        {currentPost.downvotes} downvotes
                     </div>
                 </div>
                 <div className={styles.blogPostImageContainer}>
                     <img className={styles.blogPostImage} alt="" src={blogPic}></img>
                 </div>
                 <div className={styles.blogPostContent}>
-                    {this.state.post.body}
+                    {currentPost.body}
                 </div>
             </div>
         );
     }
 }
-export default BlogPostContent;
+const mapStateToProps = (state, ownProps) => {
+    return {
+        blogPosts: state.blogPosts
+    }
+}
+export default connect(mapStateToProps)(BlogPostContent);
--- src/components/BlogPostPage/blogPostPage.js
+++ src/components/BlogPostPage/blogPostPage.js
@@ -1,50 +1,19 @@
 import React from "react"
-import { auth } from "../../config/firebase"
-
 import Layout from "../../components/Layout/layout"
 import BlogPost from "../../components/BlogPost/blogPost"
 // import Sidebar from "../components/sidebar"
 import styles from "./blogPostPage.module.scss"
-import Loader from "../../components/Loader/loader"
 
 class BlogPostPage extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            loading: true,
-            loggedInUser: {}
-        }
-    }
-
-    componentDidMount() {
-        auth.onAuthStateChanged((user) => {
-            let userEmail = user.email;
-            let username = user.displayName;
-            let userImageURL = user.photoURL;
-            this.setState({
-                loading: false,
-                loggedInUser: {
-                    userEmail: userEmail,
-                    username: username,
-                    userImageURL: userImageURL
-                }
-            })
-        });
-    }
     render () {
-        if(this.state.loading) {
-            return <Loader></Loader>
-        }
-        else {
-            return (
-                <Layout>
-                    <div className={styles.blogPostPage}>
-                        <BlogPost postState={this.props.location.state}></BlogPost>
-                        {/* <Sidebar></Sidebar> */}
-                    </div>
-                </Layout>
-            );
-        }
+        return (
+            <Layout>
+                <div className={styles.blogPostPage}>
+                    <BlogPost postState={this.props.location.state}></BlogPost>
+                    {/* <Sidebar></Sidebar> */}
+                </div>
+            </Layout>
+        );
     }
 }
 export default BlogPostPage;
